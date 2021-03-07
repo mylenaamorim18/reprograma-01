e0c9ade241d87e59bee688e045e3894241a055da
mylenaamorim18 <mylenaamorim18@hotmail.com>
2021-03-07 14:29:11 -0300

    Primeiro commit

---
+++ calculadora/js/script.js
@@ -0,0 +1,18 @@
+var inputGanhoPorMes = document.querySelector("#ganho-mes");
+
+var inputHorasPorDia = document.querySelector("#horas-dia");
+
+var spanHora = document.querySelector("#resposta")
+
+console.log(inputHorasPorDia);
+
+function calcularValorHora() {
+    var qtdDeHorasMes = inputHorasPorDia.valueAsNumber * 22
+
+    var valorDaHora = (inputGanhoPorMes.valueAsNumber / qtdDeHorasMes).toFixed(2)
+
+    spanHora.innerHTML = "R$" + valorDaHora
+
+}
+
+
